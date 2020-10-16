e3a77d58ed61794a3038cd6f18813d9a2211c9f8
Ivan Kravchenko <41616785+TheKeyboardist@users.noreply.github.com>
2020-10-16 15:41:07 -0400

    finished lab

---
+++ Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class BulletFactory : MonoBehaviour
+{
+    [Header("Bullet Types")]
+    public GameObject regularBullet;
+    public GameObject fatBullet;
+    public GameObject pulsingBullet;
+
+
+    public BulletManager BulletManager;
+
+    public GameObject createBullet(BulletType type = BulletType.RANDOM)
+    {
+        if(type == BulletType.RANDOM)
+        {
+
+            var randomBullet = Random.Range(0, 3);
+            type = (BulletType) randomBullet;
+        }
+
+
+
+
+        GameObject tempBullet = null;
+        switch (type)
+        {
+            case BulletType.REGULAR:
+                tempBullet = Instantiate(regularBullet);
+                tempBullet.GetComponent<BulletController>().damage = 10;
+                break;
+            case BulletType.FAT:
+                tempBullet = Instantiate(fatBullet);
+                tempBullet.GetComponent<BulletController>().damage = 15;
+                break;
+            case BulletType.PULSING:
+                tempBullet = Instantiate(pulsingBullet);
+                tempBullet.GetComponent<BulletController>().damage = 30;
+                break;
+        }
+
+        tempBullet.transform.parent = transform;
+        tempBullet.SetActive(false);
+
+        return tempBullet;
+    }
+
+}
